b6621d13f6b148743099456e1f6bc10eb2444cec
Yael Peet <yaelpeet@gmail.com>
2025-02-23 21:42:47 -0800

    nav bar hell

--- app.js
+++
@@ -1,7 +0,0 @@
-// app.js
-const hamburgerMenu = document.getElementById('hamburger-menu');
-const navList = document.getElementById('nav-list');
-
-hamburgerMenu.addEventListener('click', () => {
-  navList.classList.toggle('open');
-});
